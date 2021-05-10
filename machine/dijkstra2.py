d79a0b51e169a5597484cd68402496c96b599a43
Mauricio <mauricioferreirasilva13@gmail.com>
2021-05-10 18:10:31 -0300

    Organizing examples

--- machine/dijkstra2.py
+++ machine/dijkstra2.py
@@ -1,13 +1,16 @@
 import dijkstar as dj
-from machine import automata
-def graph_G(auto):
+def graph_G(auto,black,initial):
     graph = dj.Graph()
     pos = auto.transitions
     sts = auto.states_set()
     for state in sts:
         t = pos[state].keys()
         for eve in t:
-            graph.add_edge(state, pos[state][eve], eve.weight)
+            if state == initial:
+                if eve not in black:
+                    graph.add_edge(state, pos[state][eve], eve.weight)
+            else:
+                graph.add_edge(state, pos[state][eve], eve.weight)
     return graph
 def path_trans(auto, path):
     sequence = [None] * (len(path) - 1)
@@ -19,9 +22,80 @@ def path_trans(auto, path):
                 sequence[s] = trans
     return(sequence)
 
-def PATH(AUTO, INITIAL, FINAL):
-    GRAPH = graph_G(AUTO)
+def PATH(AUTO, BLACK, INITIAL, FINAL):
+    GRAPH = graph_G(AUTO, BLACK,INITIAL)
     P_calculus = dj.find_path(GRAPH, INITIAL, FINAL)
     P_nodes = P_calculus.nodes
     TRANS = path_trans(AUTO, P_nodes)
     return(TRANS, P_nodes)
+
+def graph_G2(auto,black):
+    graph = dj.Graph()
+    pos = auto.transitions
+    sts = auto.states_set()
+    for state in sts:
+        t = pos[state].keys()
+        for eve in t:
+            if eve not in black:
+                graph.add_edge(state, pos[state][eve], eve.weight)
+    return graph
+
+def PATH2(AUTO, BLACK, INITIAL, FINAL):
+    GRAPH = graph_G2(AUTO, BLACK)
+    P_calculus = dj.find_path(GRAPH, INITIAL, FINAL)
+    P_nodes = P_calculus.nodes
+    TRANS = path_trans(AUTO, P_nodes)
+    return(TRANS, P_nodes)
+
+def DIST(AUTO, INITIAL, RADIUS):
+    l = 0
+    s = [INITIAL]
+    dist = dict()
+    dist[l] = s
+    pos = AUTO.transitions
+    checked = list()
+    while l <= RADIUS:
+        dist[l+1]=list()
+        for s in dist[l]:
+            for eve in pos[s].keys():
+                s2 = pos[s][eve]
+                for L in range(l+1):
+                    for ss in dist[L]:
+                        if ss not in checked:
+                            checked.append(ss)
+                if s2 not in checked:
+                    dist[l+1].append(pos[s][eve])
+                    checked.append(s2)
+        l += 1
+    dist.pop(l)
+    s_list = list()
+    for L in range(l):
+        for ss in dist[L]:
+            s_list.append(ss)
+    return(s_list)
+
+
+def GRAPH_VISION(auto, black, initial, r):
+    d = DIST(auto, initial, r)
+    graph = dj.Graph()
+    pos = auto.transitions
+    sts = auto.states_set()
+    for state in sts:
+        t = pos[state].keys()
+        for eve in t:
+            s2 = pos[state][eve]
+            if state in d:
+                if eve not in black:
+                    graph.add_edge(state, pos[state][eve], eve.weight)
+            else:
+                graph.add_edge(state, pos[state][eve], eve.weight)
+    return graph
+
+def PATH_VISION(AUTO, BLACK, INITIAL, FINAL,RADIUS):
+    GRAPH = GRAPH_VISION(AUTO, BLACK, INITIAL,RADIUS)
+    P_calculus = dj.find_path(GRAPH, INITIAL, FINAL)
+    P_nodes = P_calculus.nodes
+    TRANS = path_trans(AUTO, P_nodes)
+    #print(P_nodes)
+    return(TRANS, P_nodes)
+
--- machine/rob_callback.py
+++ machine/rob_callback.py
@@ -23,9 +23,9 @@ def FC_POSSIBLE_STATES_ARRAY(dic_states):
 def FC_SET_ALL_POSITIONS (dic_states, position):
     position_array = np.zeros([3, len(position)])
     for b in range(len(position)):
-        position_array[0][b] = dic_states[position[b].name][0]
-        position_array[1][b] = dic_states[position[b].name][1]
-        position_array[2][b] = dic_states[position[b].name][2]
+        position_array[0][b] = dic_states[position[b]][0]
+        position_array[1][b] = dic_states[position[b]][1]
+        position_array[2][b] = dic_states[position[b]][2]
     return(position_array)
 
 def FC_MAKE_REAL_TRANSITION(possible_positions, possible_names, state_now, position_read, number_of_robot, safe_radius):
@@ -51,49 +51,14 @@ def FC_MAKE_REAL_TRANSITION(possible_positions, possible_names, state_now, posit
 
     return (state)
 
-def FC_MAKE_LOGICAL_TRANSITION(logical, n, dic_transitions, real, transitions_sequence, buffer):
-    logical[n] = dic_transitions[logical[n]][transitions_sequence[n][int(buffer[n])]]
-    buffer[n] += 1
-
-def FC_MAKE_LOGICAL_TRANSITION(logical, n, dic_transition, path):
-    logical[n] = dic_transition[path]
-
-def logical_transition(input, n, path, buffer):
-    input[n] = path[n][buffer[n]]
-    buffer[n] += 1
-
-
-def logical_states(auto, input_transition, input_state, real, n,buffer):
-    input_state[n] = auto.transitions[real[n]][input_transition[n][buffer[n]]]
-    buffer[n] += 1
-    return (input_state)
-
-def add_black(auto, blacklist, logic, N):
-    for i in range(N):
-        blocked_trans = auto.state_transition_set(logic[i])
-        for j in range(len(blocked_trans)):
-            single_transition_blocked = blocked_trans[j]
-            if single_transition_blocked not in blacklist:
-                blacklist.append(single_transition_blocked)
-    return
-def add_black2(auto, blacklist, state):
-    black = list()
-    for e, s2 in auto.transitions[state].items():
-        black.append(e)
-    for block in black:
-        if block not in blacklist:
-            blacklist.append(block)
-    return
-
 def add_black3(auto, blacklist, state):
     black = list()
     #print(state)
     k = auto.transitions.keys()
     for s in k:
         for e, s2 in auto.transitions[s].items():
-            #print(s,s2)
             if s2 == state:
-                #print(s2)
+                #print(e, s2)
                 black.append(e)
     #print(black)
     for block in black:
@@ -102,24 +67,30 @@ def add_black3(auto, blacklist, state):
         #print(blacklist)
     return blacklist
 
-def check_momentaneous_block(auto,s,blacklist):
-    goal = list()
-    for e, s2 in auto.transitions[s].items():
-        if e not in blacklist:
-            goal.append(s2)
-    if len(goal) >= 1:
-        blocked = False
+def check_block(trans, real, blacklist):
+    #print(trans[real].keys(),blacklist)
+    Events = trans[real].keys()
+    a = len(Events)
+    b = list()
+    for c in Events:
+        if c in blacklist:
+            b.append(c)#print('calcula')
+    #print(b,len(b),a)
+    if len(b) == a:
+        return(True)
+        print(b,blacklist)
+
     else:
-        blocked = True
-    #print(goal)
-    return(blocked, goal)
+        return(False)
+    #print('NAO')
+    return(True)
 
-# para iniciar, preciso:
-# para cada robô, ler o estado inicial e colocar em initial_points, além disso calcular o menor caminho possível
-# e guardar essa informação em uma lista para cada robô.
-
-# para cada robô indicar a próxima transição e bloquear as transições levando ao estado lógico
-# mas antes verificar se a transição está entre as proibidas, se estiver criar o automato sem
-# as transições bloqueadas e calcular o dijkstra
-
-#def checking_blocked_transitions
+def add_black_real_logical(auto,blacklist, real,logical):
+    black = list()
+    for ev, j2 in auto.transitions[logical].items():
+        if j2 == real:
+            black.append(ev)
+    for j3 in black:
+        if j3 not in blacklist:
+            blacklist.append(j3)
+    return(blacklist)
